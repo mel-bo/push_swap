f87ac3e4f5fef37907421a726abab92a57d74782
Mohamed El Boutakmanti <mel-bout@shi-r7-p5.s19.be>
2024-11-24 21:43:28 +0100

    phoenix push

--- operators.c
+++ operators.c
@@ -6,22 +6,25 @@
 /*   By: mel-bout <mel-bout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 19:15:56 by mel-bout          #+#    #+#             */
-/*   Updated: 2024/11/07 16:40:59 by mel-bout         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:19:43 by mel-bout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap_s(stack *s)
+void	swap_n(stack *s)
 {
 	node	*tmp;
 
-tmp = s->head;
 	if (s->size == 2)
 	{
-		tmp->x = s->head->x;
-		s->head->x = s->tail->x;
-		s->tail->x = tmp->x;
+		tmp = s->head;
+		s->head->preview = s->tail;
+		s->tail->next = s->head;
+		s->head->next = NULL;
+		s->tail->preview = NULL;
+		s->head = s->tail;
+		s->tail = tmp;
 	}
 	else
 	{
@@ -32,4 +35,63 @@ tmp = s->head;
 		tmp->preview = NULL;
 		s->head = tmp;
 	}
+}
+
+void	rotate_n(stack *s)
+{
+	node	*tmp;
+	
+	tmp = s->head->next;
+	if (s->size == 2)
+		swap_s(&s);
+	else
+	{
+		s->tail->next = s->head;
+		s->head->preview = s->tail;
+		s->head->next = NULL;
+		tmp->preview = NULL;
+		s->tail = s->head;
+		s->head = tmp;
+	}
+}
+
+void	rev_rotate(stack *s)
+{
+	node	*tmp;
+
+	tmp = s->tail->preview;
+	if (s->size == 2)
+		swap_s(&s);
+	s->head->preview = s->tail;
+	s->tail->next = s->head;
+	s->tail->preview = NULL;
+	tmp->next = NULL;
+	s->head = s->tail;
+	s->tail = tmp;
+}
+
+void	push_n(stack *x, stack *y)
+{
+	node	*tmp;
+	
+	if (!y)
+	{
+		y->head = x->head;
+		y->tail = x->head;
+		x->head = x->head->next;
+		x->head->preview = NULL;
+		y->head->preview = NULL;
+		y->head->next = NULL;
+	}
+	else
+	{
+		tmp = y->head;
+		y->head = x->head;
+		x->head = x->head->next;
+		x->head->preview = NULL;
+		y->head->next = tmp;
+		tmp->preview = y->head;
+	}
+	if (x->head == NULL)
+		x->tail = NULL;
 }
